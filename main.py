5b73a826fea6a06bddaa3600424ac663a38d6bdc
CharlieChen <new791104@gmail.com>
2017-02-07 13:45:43 +0800

    paragraph process
    完成段落處理

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ readfile = open(filename,'r')
 writefile = open("output.txt","w")
 paragraph = ""
 
-for line in open(filename,'r'):
-    line -= '\r\n'
-    paragraph += line
-    print(line)
+with open(filename) as file:
+    lines = (line.strip('\r\n') for line in file)  #lines 是底下 for迴圈 的 generator, string.strip() 是一個刪減字串的方法
+    for line in lines:
+        paragraph += line
+        print(line,end='')
+
 writefile.write(paragraph)
-#print(paragraph,end="")
 readfile.close()
 writefile.close()
 
