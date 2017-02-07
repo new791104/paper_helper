aa6f2164aa24ae7327fc6ca7dca4f0fbd8d57bde
CharlieChen <new791104@gmail.com>
2017-02-07 16:18:43 +0800

    完成核心
    完成論文小幫手應該要有的基本功能

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
 filename = "a.txt"
 
 readfile = open(filename,'r')
 writefile = open("output.txt","w")
-paragraph = ""
+paragraph_eng = ""
+paragraph_ch = ""
 
 with open(filename) as file:
     lines = (line.strip('\r\n') for line in file)  #lines 是底下 for迴圈 的 generator, string.strip() 是一個刪減字串的方法
     for line in lines:
-        paragraph += line
-        print(line,end='')
+        paragraph_eng += line
+        #print(line,end='')
 
-writefile.write(paragraph)
+driver = webdriver.PhantomJS(executable_path=r'E:\phantomjs-2.1.1-windows\bin\phantomjs')  #開啟PhantomJs瀏覽器
+driver.get('https://translate.google.com/?source=gtx#en/zh-TW/' + paragraph_eng)  # 輸入範例網址，交給瀏覽器
+res = driver.page_source  # 取得網頁原始碼
+soup = BeautifulSoup(res,"lxml")
+for drink in soup.select('{}'.format("#result_box")):
+    #print(drink.get_text())
+    paragraph_ch += drink.get_text()
+
+writefile.write(paragraph_ch)
+driver.close()  # 關閉瀏覽器
 readfile.close()
 writefile.close()
 
--- test.py
+++ test.py
@@ -1,1 +1,36 @@
-print("Hello");
+#測試用程式碼
+import http.client
+"""
+result_id = 'result_box'
+conn = http.client.HTTPSConnection("translate.google.com")
+conn.request("GET", "/?source=gtx#en/zh-TW/test")
+response = conn.getresponse()
+data = response.read()
+
+print(data.find(result_id))
+"""
+#print(response.status, response.reason)
+#strstr(sStr1,sStr)
+
+####request method####
+import requests
+
+from selenium import webdriver
+"""
+res = requests.get('https://translate.google.com/?source=gtx#en/zh-TW/test')
+print(res.text)
+
+soup = BeautifulSoup(res.text,"lxml")
+for drink in soup.select('{}'.format("#result_box")):
+    print(drink.get_text())"""
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+driver = webdriver.PhantomJS(executable_path=r'E:\phantomjs-2.1.1-windows\bin\phantomjs')  # PhantomJs
+driver.get('https://translate.google.com/?source=gtx#en/zh-TW/test')  # 輸入範例網址，交給瀏覽器
+res = driver.page_source  # 取得網頁原始碼
+soup = BeautifulSoup(res,"lxml")
+for drink in soup.select('{}'.format("#result_box")):
+    print(drink.get_text())
+
+driver.close()  # 關閉瀏覽器
